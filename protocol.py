8c72684f8c2e1b26062b54c8cfb65b0663701a75
Jo <jozellealmero@gmail.com>
2026-05-13 16:48:21 +0800

    Created computeChecksum and verifyChecksum functions

--- protocol.py
+++ protocol.py
@@ -10,3 +10,13 @@ class UDPSegment:
 
     def computeChecksum(data):
         return sum(data.encode()) % 256
+    
+    def verifyChecksum():
+        return self.computeChecksum() == self.checksum
+    
+class IPPacket:
+    def __init__(self, src_mac, dst_mac, frame_type, payload):
+        self.src_mac = src_mac
+        self.dst_mac = dst_mac
+        self.frame_type = frame_type
+        self.payload = payload
